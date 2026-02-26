f12c115a599ec7dabcab5d06f62710cac00dc60a
cavalheirolucas <cavalheiro54@gmail.com>
2026-02-26 20:40:46 -0300

    adj4

--- dags/redshift_copy_incremental.py
+++ dags/redshift_copy_incremental.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
